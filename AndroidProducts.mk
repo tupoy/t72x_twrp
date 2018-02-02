4b4591c12f5f39e6e230d6555fb0927f892a2458
tupoy <mihan_88@bk.ru>
2018-02-02 14:22:09 +0300

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,4 @@
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/T72X_Oysters.mk \
+$(LOCAL_DIR)/omni_T72X_Oysters.mk
---
+++ omni_T72X_Oysters.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/Oysters/T72X_Oysters/T72X_Oysters.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := T72X_Oysters
+PRODUCT_NAME := omni_T72X_Oysters
+PRODUCT_BRAND := Oysters
+PRODUCT_MODEL := Boost 2se
+PRODUCT_MANUFACTURER := Oysters
